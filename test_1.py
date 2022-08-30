c8bd2856959bb4ee89a564eed72184ac4c3cb701
zekori1 <ukrop1993@mail.ru>
2022-08-31 01:48:31 +0300

    сделал все методы

--- test_1.py
+++ test_1.py
@@ -1,6 +1,5 @@
 import pprint
 import json
-
 import requests
 
 
@@ -19,16 +18,17 @@ def test_post_v1_account():
     response = requests.post('http://localhost:5051/v1/account', headers=headers, json=json_data)
 
 
-def test_post_v1_account_user_2():
+def test_post_v1_account_user_3():
     headers = {
         'accept': '*/*',
-
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
     }
 
     json_data = {
-        'login': 'test_user_2',
-        'email': 'test_user_2@mail.ru',
-        'password': 'test_user_2',
+        'login': 'test_user_3',
+        'email': 'test_user_3@mail.ru',
+        'password': 'test_user_3',
     }
 
     response = requests.post('http://localhost:5051/v1/account', headers=headers, json=json_data)
@@ -57,7 +57,20 @@ def test_post_v1_account_login():
 
     response = requests.post('http://localhost:5051/v1/account/login', headers=headers, json=json_data)
 
+def test_post_v1_account_login_3():
+    headers = {
+        'accept': 'text/plain',
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
+    }
 
+    json_data = {
+        'login': 'test_user_3',
+        'password': 'test_user_3',
+        'rememberMe': True,
+    }
+
+    response = requests.post('http://localhost:5051/v1/account/login', headers=headers, json=json_data)
 def test_get_v1_account():
     headers = {
         'accept': 'text/plain',
@@ -101,13 +114,59 @@ def test_get_all_email():
     body = content['Body']
     qson = json.loads(body)
     print(qson)
-    #confirmation_link_url = qson['ConfirmationLinkUrl']
-
-    # token = confirmation_link_url.split('/')[-1]
-    # print()
-    # pprint.pprint(token)
-    #token = response.json()['items'][0]['Content']['Body']['ConfirmationLinkUrl'].split('/')[-1]
-#    return token
-
+    confirmation_link_url = qson['ConfirmationLinkUri']
+    token = confirmation_link_url.split('/')[-1]
+    print()
+    pprint.pprint(token)
+    #token = response.json()['items'][0]['Content']['Body']['ConfirmationLinkUri'].split('/')[-1]
+    return token
 
 
+def test_post_v1_account_password():
+    headers = {
+        'accept': 'text/plain',
+        'X-Dm-Auth-Token': '364bddc5-b80d-4283-b179-52e7b602560f',
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'login': 'test_user_1',
+        'email': 'test_user_1@mail.ru',
+    }
+
+    response = requests.post('http://localhost:5051/v1/account/password', headers=headers, json=json_data)
+
+def test_put_v1_account_email():
+
+    headers = {
+        'accept': 'text/plain',
+        'X-Dm-Auth-Token': 'a63777e4-a7a9-42a5-a46c-a6261a45daae',
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'login': 'test_user_1',
+        'password': 'test_user_1',
+        'email': 'test_user_1@mail.ru',
+    }
+
+    response = requests.put('http://localhost:5051/v1/account/email', headers=headers, json=json_data)
+
+
+def test_delete_v1_account_login():
+    headers = {
+        'accept': '*/*',
+        'X-Dm-Auth-Token': 'IQJh+zgzF5BuAn757nBOvgrFuqIcTY7g35gSlaGbwJsn1xnVTi5xt3xw3C9uGRGo/Mqv8USCAnLoiXihAEwsC1uT8Bt8YzNJcsQ+9rZ0atx3M6UzjnsgWyWQLx889l/bbSvCXkF037o= ',
+    }
+
+    response = requests.delete('http://localhost:5051/v1/account/login', headers=headers)
+
+def test_DELETE_v1_account_login_all():
+    headers = {
+        'accept': '*/*',
+        'X-Dm-Auth-Token': 'IQJh+zgzF5BuAn757nBOvgrFuqIcTY7g35gSlaGbwJsn1xnVTi5xt3xw3C9uGRGo/Mqv8USCAnJFQfktudIgi6WD7hkEI7Ql3rEDrV+zQqbiNO4VcW+Zde8cVlIlQqI5A6/wLA+My0U= ',
+    }
+
+    response = requests.delete('http://localhost:5051/v1/account/login/all', headers=headers)
