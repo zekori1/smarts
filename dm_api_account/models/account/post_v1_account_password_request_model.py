89efa52b569068418d092e5e7790b70e11464e44
zekori1 <ukrop1993@mail.ru>
2022-09-23 00:32:44 +0300

    начал писать тесты для форума

--- dm_api_account/models/account/post_v1_account_password_request_model.py
+++ dm_api_account/models/account/post_v1_account_password_request_model.py
@@ -1,33 +1,7 @@
 from jsonmodels.models import Base
-from jsonmodels.fields import StringField, IntField, FloatField, EmbeddedField, ListField, BoolField, DateField
+from jsonmodels.fields import StringField
 
 
-class Rating(Base):
-    enabled = BoolField()
-    quality = IntField()
-    quantity = IntField()
-
-
-def roles_account_password_validator(value):
-    some = ['Guest', 'Player', 'Administrator', 'NannyModerator', 'RegularModerator', 'SeniorModerator']
-    for item in value:
-        if item not in some:
-            raise ValueError(f'Значение {item} не в списке допустимых значений some')
-
-
-class User(Base):
+class ResetPasswordResponseModel(Base):
     login = StringField()
-    roles = ListField(str, validators=[roles_account_password_validator])
-    medium_picture_url = StringField(name='mediumPictureUrl')
-    small_picture_url = StringField(name='smallPictureUrl')
-    status = StringField()
-    rating = EmbeddedField(Rating)
-    online = DateField()
-    name = StringField()
-    location = StringField()
-    registration = DateField()
-
-
-class UserEnvelopeResponseModel(Base):
-    resource = EmbeddedField(User)
-    metadata = StringField()
+    email = StringField()
--- dm_api_account/models/account/put_v1_account_password_request_model.py
+++ dm_api_account/models/account/put_v1_account_password_request_model.py
@@ -1,33 +1,9 @@
 from jsonmodels.models import Base
-from jsonmodels.fields import StringField, IntField, FloatField, EmbeddedField, ListField, BoolField, DateField
+from jsonmodels.fields import StringField
 
 
-class Rating(Base):
-    enabled = BoolField()
-    quality = IntField()
-    quantity = IntField()
-
-
-def roles_put_account_password_validator(value):
-    some = ['Guest', 'Player', 'Administrator', 'NannyModerator', 'RegularModerator', 'SeniorModerator']
-    for item in value:
-        if item not in some:
-            raise ValueError(f'Значение {item} не в списке допустимых значений some')
-
-
-class User(Base):
+class ChangePasswordResponseModel(Base):
     login = StringField()
-    roles = ListField(str, validators=[roles_put_account_password_validator])
-    medium_picture_url = StringField(name='mediumPictureUrl')
-    small_picture_url = StringField(name='smallPictureUrl')
-    status = StringField()
-    rating = EmbeddedField(Rating)
-    online = DateField()
-    name = StringField()
-    location = StringField()
-    registration = DateField()
-
-
-class UserEnvelopeResponseModel(Base):
-    resource = EmbeddedField(User)
-    metadata = StringField()
+    token = StringField()
+    old_password = StringField(name='oldPassword')
+    new_password = StringField(name='newPassword')
